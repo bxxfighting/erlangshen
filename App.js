527f8a0ba4da61fb19f89edf25bd820a6ee4de16
xx <305526954@qq.com>
2018-03-01 16:58:51 +0800

    现在可以来回的加密解密，并且增加新的项目了

--- App.js
+++ App.js
@@ -2,48 +2,166 @@ import React, {Component} from 'react';
 import {FlatList, ListView, Alert, Button, Text, TextInput, View} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 
+var CryptoJS = require("crypto-js")
+
+class AddAccountItemScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'key': '',
+            'platform': '',
+            'username': '',
+            'password': '',
+        };
+    }
+
+    add_account_item() {
+        fetch('http://www.buxingxing.com/erlangshen/account/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            'body': JSON.stringify({
+                platform: CryptoJS.AES.encrypt(this.state.platform, this.state.key).toString(),
+                username: CryptoJS.AES.encrypt(this.state.username, this.state.key).toString(),
+                password: CryptoJS.AES.encrypt(this.state.password, this.state.key).toString(),
+            })
+        })
+    }
+
+    componentWillMount() {
+        const {params} = this.props.navigation.state;
+        const key = params ? params.key : '';
+        this.setState({
+            key: key,
+        })
+    }
+
+    render() {
+        return (
+            <View
+                style={{
+                    flex:1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <TextInput
+                    style={{height: 25}}
+                    placeholder="平台"
+                    onChangeText={(text) => this.setState({platform: text})}
+                />
+                <TextInput
+                    style={{height: 25}}
+                    placeholder="账号"
+                    onChangeText={(text) => this.setState({username: text})}
+                />
+                <TextInput
+                    style={{height: 25}}
+                    placeholder="密码"
+                    onChangeText={(text) => this.setState({password: text})}
+                />
+                <Button
+                    title="添加"
+                    onPress={()=>{
+                        this.add_account_item();
+                        this.props.navigation.goBack();
+                    }}
+                />
+            </View>
+        )
+    }
+}
 
 class AccountItemsScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
             'account_list': [],
+            'key': '',
         };
     }
+
+
+    static navigationOptions = {
+        headerRight: (
+            <Button
+                title="+"
+                onPress={() => {
+                    this.props.navigation.navigate('AddAccountItem', {
+                        key: this.state.key,
+                    });
+                }}
+            />
+        )
+    }
+
     getAccountList() {
-        fetch('http://www.buxingxing.com/erlangshen/account/list/')
+        var account_list = [];
+        fetch('http://www.buxingxing.com/erlangshen/account/')
             .then((response) => response.json())
             .then((responseJson) => {
-                console.log(responseJson);
-                this.setState({
-                    account_list: responseJson.data.account_list,
-                })
+                for (let account of responseJson.data.account_list) {
+                    var platform = CryptoJS.AES.decrypt(account.platform, this.state.key).toString(CryptoJS.enc.Utf8);
+                    var username = CryptoJS.AES.decrypt(account.username, this.state.key).toString(CryptoJS.enc.Utf8);
+                    var password = CryptoJS.AES.decrypt(account.password, this.state.key).toString(CryptoJS.enc.Utf8);
+                    account_list.push({
+                        'platform': platform,
+                        'username': username,
+                        'password': password,
+                    })
+                    this.setState({
+                        account_list: account_list,
+                    })
+                }
             })
     }
+
     componentWillMount() {
         this.getAccountList();
-    }
-    render() {
         const {params} = this.props.navigation.state;
         const key = params ? params.key : '';
+        this.setState({
+            key: key,
+        })
+    }
+
+    render() {
+        for (let account of this.state.account_list) {
+            console.log(account.platform);
+            console.log(account.username);
+            console.log(account.password);
+        }
         return (
-                <FlatList
-                    data={this.state.account_list}
-                    renderItem={({item}) =>
-                        <View
-                            style={{
-                                flex:1,
-                                flexDirection: 'row',
-                                justifyContent: 'space-between',
-                                alignItems: 'center'
-                            }}
-                        >
-                            <Text>{item.platform}</Text>
-                            <Text>{item.username}</Text>
-                            <Text>{item.password}</Text>
-                        </View>
-                    }
-                />
+                <View>
+                    <FlatList
+                        data={this.state.account_list}
+                        renderItem={({item}) =>
+                            <View
+                                style={{
+                                    flex:1,
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                <Text>{item.platform}</Text>
+                                <Text>{item.username}</Text>
+                                <Text>{item.password}</Text>
+                            </View>
+                        }
+                    />
+                    <Button
+                        title="+"
+                        onPress={() => {
+                            this.props.navigation.navigate('AddAccountItem', {
+                                key: this.state.key,
+                            });
+                        }}
+                    />
+                </View>
         );
     }
 }
@@ -100,6 +218,9 @@ const RootStack = StackNavigator (
         },
         AccountItems: {
             screen: AccountItemsScreen,
+        },
+        AddAccountItem: {
+            screen: AddAccountItemScreen,
         },
     },
     {
